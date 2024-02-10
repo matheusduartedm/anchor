6aee7760b04600a66a3a3674511af8afe50150a6
matheusduartedm <matheusduarte@poli.ufrj.br>
2024-02-10 12:23:45 -0300

    First commit

--- examples/tutorial/basic-1/programs/basic-1/src/lib.rs
+++ examples/tutorial/basic-1/programs/basic-1/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("2CDGMTweWuuj79PLHSKkXXnwvrHt2LZEKpyW19WT2wP8");
 
 #[program]
 mod basic_1 {
